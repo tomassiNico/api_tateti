2bb72f4be150779b3cf1668ab0b367ebc15239cc
tomassiNico <xtomassix@gmail.com>
2019-08-31 16:39:50 -0300

    ruta para obtener nueva partida y para generar nueva partida

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, redirect
 
 app = Flask(__name__)
+
+partida = {"jugador":"Luis Alberto", "forma":"X", "tablero":",,,,,,,,,", "ganador":""}
 
 @app.route("/")
 def hello():
     return "Hello world!"
 
+@app.route('/partida')
+def get_partida():
+    return partida
+
+@app.route('/nueva_partida')
+def nueva_partida():
+    partida = {"jugador":"Luis Alberto", "forma":"X", "tablero":",,,,,,,,,", "ganador":""}
+    return redirect('/partida')
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
