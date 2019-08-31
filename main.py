8714dd0196891bf1262918169e9bb028b04bc2c7
tomassiNico <xtomassix@gmail.com>
2019-08-31 16:47:27 -0300

    funcion para verificar si hay ganador

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ app = Flask(__name__)
 
 partida = {"jugador":"Luis Alberto", "forma":"X", "tablero":",,,,,,,,,", "ganador":""}
 
+def hay_ganador(tablero):
+    #paso de string a lista
+    tab = tablero.split(",")
+    #genero lista con combinaciones ganadoras
+    pos_ganadoras = [[0,1,2],[3,4,5],[6.7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
+    for pos in pos_ganadoras:
+        if (tab[pos[0]] == '') or (tab[pos[1]] == '') or (tab[pos[2]] == ''):
+            # si ingresa aca significa que hay al menos una posicion que esta vacia por lo tanto no hay posibilidad de ganar
+            # sigo con la siguiente posible posicion ganadora
+            continue
+        if (tab[pos[0]] == tab[pos[1]] == tab[pos[2]]):
+            return True
+    return False
+
+
 @app.route("/")
 def hello():
     return "Hello world!"
